38cec79be2cff416775f726ba414f7412247b6a8
Elegant Software Engineer <elegantdev199742@gmail.com>
2022-04-06 23:55:08 +0100

    cleanup

--- data/db.js
+++ data/db.js
@@ -3,7 +3,6 @@ const knex = require('knex');
 const knexfile = require('../knexfile');
 
 const env = process.env.NODE_ENV || 'development';
-console.info('node_env', env);
 
 const configOptions = knexfile[env];
 module.exports = knex(configOptions);

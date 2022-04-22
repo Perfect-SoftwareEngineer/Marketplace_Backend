d72e354c8da3c20ec36b9ce303fb88080e073873
Elegant Software Engineer <elegantdev199742@gmail.com>
2022-04-22 15:14:52 +0100

    Updated pm2 to use env config

--- staging_run_script.sh
+++ staging_run_script.sh
@@ -3,4 +3,4 @@ echo $NODE_ENV
 npm install
 pm2 stop marketplace_app
 npm run migrate
-NODE_ENV=$NODE_ENV; PORT=$PORT; DB_URL=$DB_URL; MORALIS_SERVER_URL=$MORALIS_SERVER_URL; MORALIS_APP_ID=$MORALIS_APP_ID; pm2 start ./src/bin/www/index.js --name marketplace_app
+NODE_ENV=$NODE_ENV PORT=$PORT DB_URL=$DB_URL MORALIS_SERVER_URL=$MORALIS_SERVER_URL MORALIS_APP_ID=$MORALIS_APP_ID pm2 start ./src/bin/www/index.js --name marketplace_app

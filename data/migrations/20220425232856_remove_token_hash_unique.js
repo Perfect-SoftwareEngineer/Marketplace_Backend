d752dd01891418cb574002812a594d95b1c7cc6d
Elegant Software Engineer <elegantdev199742@gmail.com>
2022-04-26 01:20:49 +0100

    Set unique on contract_address and token_id

--- data/migrations/20220425232856_remove_token_hash_unique.js
+++ data/migrations/20220425232856_remove_token_hash_unique.js
@@ -8,9 +8,8 @@ exports.up = async function (knex) {
   await knex.schema.alterTable(dbTables.nftItems, table => {
     table.dropUnique(['contract_address', 'token_hash']);
     table.string('token_hash').nullable().alter();
-    table.unique(['collection_id', 'token_id']);
+    table.unique(['contract_address', 'token_id']);
   });
-
 };
 
 /**
